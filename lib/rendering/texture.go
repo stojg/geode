7994b3c902b5e563969e01419a2e6d2aff36894a
Stig Lindqvist <stojg.lindqvist@gmail.com>
2017-11-26 08:58:24 +1300

    Add trilinear mipmap filtering to the mesh textures

--- lib/rendering/texture.go
+++ lib/rendering/texture.go
@@ -67,6 +67,7 @@ func LoadTexture(filename string) (*TextureResource, error) {
 	// flip it into open GL format
 	rgba = flip(rgba)
 
+	// @todo this is handy when dealing with HDR/nono SRGB image sources
 	//var internalFormat int32 = gl.RGBA
 	//if gammaCorrect {
 	//	internalFormat = gl.SRGB8_ALPHA8
@@ -81,10 +82,12 @@ func LoadTexture(filename string) (*TextureResource, error) {
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
 
-	gl.TexParameterf(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+	gl.TexParameterf(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR)
 	gl.TexParameterf(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
 
 	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, resource.width, resource.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(rgba.Pix))
+
+	gl.GenerateMipmap(gl.TEXTURE_2D)
 
 	return resource, nil
 }
