d6ac7cdd94ff9ba00412d9373980a8c71d20dd86
Stig Lindqvist <stojg.lindqvist@gmail.com>
2017-12-06 15:01:21 +1300

    Clear debug texture properly

--- lib/rendering/debugger/debugger.go
+++ lib/rendering/debugger/debugger.go
@@ -28,6 +28,9 @@ func New(width, height int) {
 }
 
 func Clear() {
+	texture.BindAsRenderTarget()
+	clearColor := [4]uint32{0, 0, 0, 0}
+	gl.ClearBufferuiv(gl.COLOR, 0, &clearColor[0])
 	numPanels = 0
 }
 
