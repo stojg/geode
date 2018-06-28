1b20cc3f0fce1e2fbf5e9abc2f734f0a224d9907
Stig Lindqvist <stojg.lindqvist@gmail.com>
2018-06-28 19:15:57 +1200

    add some terrain benchmarks

--- lib/rendering/terrain/terrain_test.go
+++ lib/rendering/terrain/terrain_test.go
@@ -2,8 +2,23 @@ package terrain
 
 import "testing"
 
-func BenchmarkNew(b *testing.B) {
+// https://medium.com/@hackintoshrao/daily-code-optimization-using-benchmarks-and-profiling-in-golang-gophercon-india-2016-talk-874c8b4dc3c5
+
+//BenchmarkNew-8   	50000000	        25.5 ns/op
+func BenchmarkGetNoise(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		getNoise(0, 0)
+	}
+}
+
+func BenchmarkInterpolate(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		interpolate(2.4, 3.5, 0.5)
+	}
+}
+
+func BenchmarkGenerateTerrain(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		generateTerrain(0, 0)
 	}
 }
