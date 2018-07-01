588acb1ad052e9d5dfcb9087a14dec9fd4fd4209
Stig Lindqvist <stojg.lindqvist@gmail.com>
2018-07-02 08:35:06 +1200

    blur improvement

--- lib/rendering/postprocess/renderer.go
+++ lib/rendering/postprocess/renderer.go
@@ -12,16 +12,16 @@ func New(s components.RenderState) *Renderer {
 
 	const blurDownScale = 4
 	return &Renderer{
-		RenderState:   s,
-		sourceTexture: framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width, components.Height, gl.RGBA16F, gl.RGBA, gl.FLOAT, gl.NEAREST, false),
-		scratch1:      framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width/2, components.Height/2, gl.RGBA16F, gl.RGB, gl.FLOAT, gl.NEAREST, false),
-		scratch2:      framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width, components.Height, gl.RGBA16F, gl.RGB, gl.FLOAT, gl.NEAREST, false),
-		blur1:         framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width/blurDownScale, components.Height/blurDownScale, gl.RGB, gl.RGB, gl.FLOAT, gl.LINEAR, false),
-		blur2:         framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width/blurDownScale, components.Height/blurDownScale, gl.RGB, gl.RGB, gl.FLOAT, gl.LINEAR, false),
-		blur3:         framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width/blurDownScale/2, components.Height/blurDownScale/2, gl.RGB, gl.RGB, gl.FLOAT, gl.LINEAR, false),
-		blur4:         framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width/blurDownScale/2, components.Height/blurDownScale/2, gl.RGB, gl.RGB, gl.FLOAT, gl.LINEAR, false),
-		blur5:         framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width/blurDownScale/4, components.Height/blurDownScale/4, gl.RGB, gl.RGB, gl.FLOAT, gl.LINEAR, false),
-		blur6:         framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width/blurDownScale/4, components.Height/blurDownScale/4, gl.RGB, gl.RGB, gl.FLOAT, gl.LINEAR, false),
+		RenderState:       s,
+		sourceTexture:     framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width, components.Height, gl.RGBA16F, gl.RGBA, gl.FLOAT, gl.NEAREST, false),
+		brightPassTexture: framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width/2, components.Height/2, gl.RGBA16F, gl.RGB, gl.FLOAT, gl.NEAREST, false),
+		scratch2:          framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width, components.Height, gl.RGBA16F, gl.RGB, gl.FLOAT, gl.NEAREST, false),
+		blur1:             framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width/blurDownScale, components.Height/blurDownScale, gl.RGB, gl.RGB, gl.FLOAT, gl.LINEAR, false),
+		blur2:             framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width/blurDownScale, components.Height/blurDownScale, gl.RGB, gl.RGB, gl.FLOAT, gl.LINEAR, false),
+		blur3:             framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width/blurDownScale/2, components.Height/blurDownScale/2, gl.RGB, gl.RGB, gl.FLOAT, gl.LINEAR, false),
+		blur4:             framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width/blurDownScale/2, components.Height/blurDownScale/2, gl.RGB, gl.RGB, gl.FLOAT, gl.LINEAR, false),
+		blur5:             framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width/blurDownScale/4, components.Height/blurDownScale/4, gl.RGB, gl.RGB, gl.FLOAT, gl.LINEAR, false),
+		blur6:             framebuffer.NewTexture(gl.COLOR_ATTACHMENT0, components.Width/blurDownScale/4, components.Height/blurDownScale/4, gl.RGB, gl.RGB, gl.FLOAT, gl.LINEAR, false),
 
 		toneMapShader: shader.NewShader("filter_tonemap"),
 		gaussShader:   shader.NewShader("filter_gauss"),
@@ -33,31 +33,31 @@ func New(s components.RenderState) *Renderer {
 
 type Renderer struct {
 	components.RenderState
-	sourceTexture *framebuffer.Texture
-	scratch1      *framebuffer.Texture
-	scratch2      *framebuffer.Texture
-	blur1         *framebuffer.Texture
-	blur2         *framebuffer.Texture
-	blur3         *framebuffer.Texture
-	blur4         *framebuffer.Texture
-	blur5         *framebuffer.Texture
-	blur6         *framebuffer.Texture
-	brightnessTex *framebuffer.Texture
-	toneMapShader *shader.Shader
-	gaussShader   *shader.Shader
-	brightness    *shader.Shader
-	combine       *shader.Shader
-	pass          *shader.Shader
+	sourceTexture     *framebuffer.Texture
+	brightPassTexture *framebuffer.Texture
+	scratch2          *framebuffer.Texture
+	blur1             *framebuffer.Texture
+	blur2             *framebuffer.Texture
+	blur3             *framebuffer.Texture
+	blur4             *framebuffer.Texture
+	blur5             *framebuffer.Texture
+	blur6             *framebuffer.Texture
+	brightnessTex     *framebuffer.Texture
+	toneMapShader     *shader.Shader
+	gaussShader       *shader.Shader
+	brightness        *shader.Shader
+	combine           *shader.Shader
+	pass              *shader.Shader
 }
 
 func (r *Renderer) Render(in *framebuffer.Texture) {
-
-	in.ResolveToFBO(r.sourceTexture)
 	gl.Disable(gl.DEPTH_TEST)
-	//gl.Viewport(0, 0, e.width, e.height)
+	in.ResolveToFBO(r.sourceTexture)
 
-	r.applyFilter(r.brightness, r.sourceTexture, r.scratch1)
-	res1 := r.blur(r.scratch1, r.blur1, r.blur2)
+	r.applyFilter(r.brightness, r.sourceTexture, r.brightPassTexture)
+
+	r.gaussShader.Bind()
+	res1 := r.blur(r.brightPassTexture, r.blur1, r.blur2)
 	res2 := r.blur(res1, r.blur3, r.blur4)
 	res3 := r.blur(res2, r.blur5, r.blur6)
 
@@ -65,6 +65,7 @@ func (r *Renderer) Render(in *framebuffer.Texture) {
 	r.SetTexture("x_filterTexture3", res2)
 	r.SetTexture("x_filterTexture4", res3)
 	r.applyFilter(r.combine, r.sourceTexture, r.scratch2)
+
 	r.applyFilter(r.toneMapShader, r.scratch2, nil)
 }
 
@@ -80,12 +81,14 @@ func (r *Renderer) blur(in, t1, t2 *framebuffer.Texture) *framebuffer.Texture {
 			scale = [3]float32{0, 1, 0}
 		}
 		r.SetVector3f("x_blurScale", scale)
+		r.SetTexture("x_filterTexture", from)
 		if initial {
-			r.applyFilter(r.gaussShader, in, to)
+			r.SetTexture("x_filterTexture", in)
 			initial = false
-		} else {
-			r.applyFilter(r.gaussShader, from, to)
 		}
+		to.BindFrameBuffer()
+		r.gaussShader.UpdateUniforms(nil, r)
+		primitives.DrawQuad()
 		x = !x
 		from, to = to, from
 	}
